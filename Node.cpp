b04aef4e598f1b2fd319297056013587cf0491a5
Harelro14 <Harelroth10@gmail.com>
2025-06-26 20:39:10 +0300

    Move functions to cpp

--- Node.cpp
+++ Node.cpp
@@ -1,5 +1,22 @@
-//
-// Created by nadav on 26/06/2025.
-//
 
 #include "Node.h"
+int Node::getId() const {
+    return this->id;
+}
+int Node::getGenreChanges() const {
+    return this->genreChanges;
+}
+void Node::setGenreChanges(int changes) {
+    this->genreChanges = changes;
+}
+void Node::updateGenreChange(){
+    this->genreChanges++;
+}
+
+Node* Node::getParent() const {
+    return parent;
+}
+
+void Node::setParent(Node* parent){
+    this->parent = parent;
+}
--- Node.h
+++ Node.h
@@ -7,36 +7,14 @@ class Node {
         int genreChanges;
 
     public:
-        explicit Node(int id) : id(id), parent(nullptr),genreChanges(0) {}
+        explicit Node(int id) : id(id), parent(nullptr),genreChanges(0){}
         ~Node() = default;
-        Node* getParent();
-        int getGenreChanges();
-        int getId();
+        Node* getParent() const;
+        int getGenreChanges() const;
+        int getId() const;
         void updateGenreChange();
         void setGenreChanges(int changes);
         void setParent(Node* parent);
-    };
-
-    int Node::getId() {
-        return this->id;
-    }
-    int Node::getGenreChanges() {
-        return this->genreChanges;
-    }
-    void Node::setGenreChanges(int changes) {
-        this->genreChanges = changes;
-    }
-    void Node::updateGenreChange(){
-        this->genreChanges++;
-    }
-
-    Node<T> *Node::getParent() {
-        return parent;
-    }
-
-    void Node::setParent(Node* parent){
-        this->parent = parent;
-    }
 };
 
 
